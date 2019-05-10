de2df0e762265753721702ebc94b6fad16111d3e
Marco Hartmann <marcohartmann95@gmail.com>
2019-05-10 16:50:02 +0200

    Cleanup different heartbeat handling functions

--- ssl/t1_lib_export.c
+++ ssl/t1_lib_export.c
@@ -8,54 +8,38 @@
 
 
 /* SETUP */
-#define ORIG 1
-#define RUST 2
-#define RUST_UNSAFE 3
+#define C_VULN 1
+#define C_FIX 2
+#define RUST 3
+#define RUST_UNSAFE 4
 
-#define MODE RUST_UNSAFE
+#define MODE RUST
 
 
 /*******************************************************************************
  * The original C function
  *******************************************************************************/
-#if MODE == ORIG
-
-/* with/without fix */
-#define VULN
-
-int tls1_process_heartbeat(SSL *s)
-{
+#if MODE == C_VULN
+int
+tls1_process_heartbeat(SSL *s)
+	{
 	unsigned char *p = &s->s3->rrec.data[0], *pl;
 	unsigned short hbtype;
 	unsigned int payload;
 	unsigned int padding = 16; /* Use minimum padding */
 
-#ifdef VULN
 	/* Read type and payload length first */
 	hbtype = *p++;
 	n2s(p, payload);
 	pl = p;
-#endif
 
 	if (s->msg_callback)
 		s->msg_callback(0, s->version, TLS1_RT_HEARTBEAT,
 			&s->s3->rrec.data[0], s->s3->rrec.length,
 			s, s->msg_callback_arg);
 
-
-#ifndef VULN
-	/* Read type and payload length first */
-	if (1 + 2 + 16 > s->s3->rrec.length)
-		return 0; /* silently discard */
-	hbtype = *p++;
-	n2s(p, payload);
-	if (1 + 2 + payload + 16 > s->s3->rrec.length)
-		return 0; /* silently discard per RFC 6520 sec. 4 */
-	pl = p;
-#endif
-
 	if (hbtype == TLS1_HB_REQUEST)
-	{
+		{
 		unsigned char *buffer, *bp;
 		int r;
 
@@ -65,7 +49,7 @@ int tls1_process_heartbeat(SSL *s)
 		 */
 		buffer = OPENSSL_malloc(1 + 2 + payload + padding);
 		bp = buffer;
-
+		
 		/* Enter response type, length and copy payload */
 		*bp++ = TLS1_HB_RESPONSE;
 		s2n(payload, bp);
@@ -85,16 +69,16 @@ int tls1_process_heartbeat(SSL *s)
 
 		if (r < 0)
 			return r;
-	}
+		}
 	else if (hbtype == TLS1_HB_RESPONSE)
 		{
 		unsigned int seq;
-
+		
 		/* We only send sequence numbers (2 bytes unsigned int),
 		 * and 16 random bytes, so we just try to read the
 		 * sequence number */
 		n2s(pl, seq);
-
+		
 		if (payload == 18 && seq == s->tlsext_hb_seq)
 			{
 			s->tlsext_hb_seq++;
@@ -103,8 +87,82 @@ int tls1_process_heartbeat(SSL *s)
 		}
 
 	return 0;
-}
+	}
+#endif
 
+#if MODE == C_FIX
+int
+tls1_process_heartbeat(SSL *s)
+	{
+	unsigned char *p = &s->s3->rrec.data[0], *pl;
+	unsigned short hbtype;
+	unsigned int payload;
+	unsigned int padding = 16; /* Use minimum padding */
+
+	if (s->msg_callback)
+		s->msg_callback(0, s->version, TLS1_RT_HEARTBEAT,
+			&s->s3->rrec.data[0], s->s3->rrec.length,
+			s, s->msg_callback_arg);
+
+	/* Read type and payload length first */
+	if (1 + 2 + 16 > s->s3->rrec.length)
+		return 0; /* silently discard */
+	hbtype = *p++;
+	n2s(p, payload);
+	if (1 + 2 + payload + 16 > s->s3->rrec.length)
+		return 0; /* silently discard per RFC 6520 sec. 4 */
+	pl = p;
+
+	if (hbtype == TLS1_HB_REQUEST)
+		{
+		unsigned char *buffer, *bp;
+		int r;
+
+		/* Allocate memory for the response, size is 1 bytes
+		 * message type, plus 2 bytes payload length, plus
+		 * payload, plus padding
+		 */
+		buffer = OPENSSL_malloc(1 + 2 + payload + padding);
+		bp = buffer;
+		
+		/* Enter response type, length and copy payload */
+		*bp++ = TLS1_HB_RESPONSE;
+		s2n(payload, bp);
+		memcpy(bp, pl, payload);
+		bp += payload;
+		/* Random padding */
+		RAND_pseudo_bytes(bp, padding);
+
+		r = ssl3_write_bytes(s, TLS1_RT_HEARTBEAT, buffer, 3 + payload + padding);
+
+		if (r >= 0 && s->msg_callback)
+			s->msg_callback(1, s->version, TLS1_RT_HEARTBEAT,
+				buffer, 3 + payload + padding,
+				s, s->msg_callback_arg);
+
+		OPENSSL_free(buffer);
+
+		if (r < 0)
+			return r;
+		}
+	else if (hbtype == TLS1_HB_RESPONSE)
+		{
+		unsigned int seq;
+		
+		/* We only send sequence numbers (2 bytes unsigned int),
+		 * and 16 random bytes, so we just try to read the
+		 * sequence number */
+		n2s(pl, seq);
+		
+		if (payload == 18 && seq == s->tlsext_hb_seq)
+			{
+			s->tlsext_hb_seq++;
+			s->tlsext_hb_pending = 0;
+			}
+		}
+
+	return 0;
+	}
 #endif
 
 
