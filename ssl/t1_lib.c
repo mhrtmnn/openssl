7812f4492a47cba46c210e24a032db6a40583ded
Marco Hartmann <marcohartmann95@gmail.com>
2019-05-13 14:06:01 +0200

    Cleanup implementation

--- ssl/t1_lib.c
+++ ssl/t1_lib.c
@@ -2551,7 +2551,202 @@ int tls1_process_sigalgs(SSL *s, const unsigned char *data, int dsize)
 
 #ifndef OPENSSL_NO_HEARTBEATS
 
-extern int tls1_process_heartbeat(SSL *s);
+/**************************************************** LBS Project *****************************************************/
+
+/**
+ * This file contains multiple implementations of the function tls1_process_heartbeat.
+ * An implementation can be chosen by assigning the respective value to HB_IMPL_CHOICE.
+ *
+ * C_VULN		: The original vulnerable implementation from release OpenSSL_1_0_1f
+ * C_FIX		: The patched implementation from release OpenSSL_1_0_1g
+ * RUST			: A reimplementation in Rust
+ * RUST_UNSAFE	: A reimplementation in Rust that deliberately uses unsafe memory copy
+ */
+
+#define C_VULN 1
+#define C_FIX 2
+#define RUST 3
+#define RUST_UNSAFE 4
+
+#define HB_IMPL_CHOICE RUST
+
+
+/*******************************************************************************
+ * The original C function
+ *******************************************************************************/
+#if HB_IMPL_CHOICE == C_VULN
+int
+tls1_process_heartbeat(SSL *s)
+	{
+	unsigned char *p = &s->s3->rrec.data[0], *pl;
+	unsigned short hbtype;
+	unsigned int payload;
+	unsigned int padding = 16; /* Use minimum padding */
+
+	/* Read type and payload length first */
+	hbtype = *p++;
+	n2s(p, payload);
+	pl = p;
+
+	if (s->msg_callback)
+		s->msg_callback(0, s->version, TLS1_RT_HEARTBEAT,
+			&s->s3->rrec.data[0], s->s3->rrec.length,
+			s, s->msg_callback_arg);
+
+	if (hbtype == TLS1_HB_REQUEST)
+		{
+		unsigned char *buffer, *bp;
+		int r;
+
+		/* Allocate memory for the response, size is 1 bytes
+		 * message type, plus 2 bytes payload length, plus
+		 * payload, plus padding
+		 */
+		buffer = OPENSSL_malloc(1 + 2 + payload + padding);
+		bp = buffer;
+
+		/* Enter response type, length and copy payload */
+		*bp++ = TLS1_HB_RESPONSE;
+		s2n(payload, bp);
+		memcpy(bp, pl, payload);
+		bp += payload;
+		/* Random padding */
+		RAND_pseudo_bytes(bp, padding);
+
+		r = ssl3_write_bytes(s, TLS1_RT_HEARTBEAT, buffer, 3 + payload + padding);
+
+		if (r >= 0 && s->msg_callback)
+			s->msg_callback(1, s->version, TLS1_RT_HEARTBEAT,
+				buffer, 3 + payload + padding,
+				s, s->msg_callback_arg);
+
+		OPENSSL_free(buffer);
+
+		if (r < 0)
+			return r;
+		}
+	else if (hbtype == TLS1_HB_RESPONSE)
+		{
+		unsigned int seq;
+
+		/* We only send sequence numbers (2 bytes unsigned int),
+		 * and 16 random bytes, so we just try to read the
+		 * sequence number */
+		n2s(pl, seq);
+
+		if (payload == 18 && seq == s->tlsext_hb_seq)
+			{
+			s->tlsext_hb_seq++;
+			s->tlsext_hb_pending = 0;
+			}
+		}
+
+	return 0;
+	}
+#endif
+
+#if HB_IMPL_CHOICE == C_FIX
+int
+tls1_process_heartbeat(SSL *s)
+	{
+	unsigned char *p = &s->s3->rrec.data[0], *pl;
+	unsigned short hbtype;
+	unsigned int payload;
+	unsigned int padding = 16; /* Use minimum padding */
+
+	if (s->msg_callback)
+		s->msg_callback(0, s->version, TLS1_RT_HEARTBEAT,
+			&s->s3->rrec.data[0], s->s3->rrec.length,
+			s, s->msg_callback_arg);
+
+	/* Read type and payload length first */
+	if (1 + 2 + 16 > s->s3->rrec.length)
+		return 0; /* silently discard */
+	hbtype = *p++;
+	n2s(p, payload);
+	if (1 + 2 + payload + 16 > s->s3->rrec.length)
+		return 0; /* silently discard per RFC 6520 sec. 4 */
+	pl = p;
+
+	if (hbtype == TLS1_HB_REQUEST)
+		{
+		unsigned char *buffer, *bp;
+		int r;
+
+		/* Allocate memory for the response, size is 1 bytes
+		 * message type, plus 2 bytes payload length, plus
+		 * payload, plus padding
+		 */
+		buffer = OPENSSL_malloc(1 + 2 + payload + padding);
+		bp = buffer;
+
+		/* Enter response type, length and copy payload */
+		*bp++ = TLS1_HB_RESPONSE;
+		s2n(payload, bp);
+		memcpy(bp, pl, payload);
+		bp += payload;
+		/* Random padding */
+		RAND_pseudo_bytes(bp, padding);
+
+		r = ssl3_write_bytes(s, TLS1_RT_HEARTBEAT, buffer, 3 + payload + padding);
+
+		if (r >= 0 && s->msg_callback)
+			s->msg_callback(1, s->version, TLS1_RT_HEARTBEAT,
+				buffer, 3 + payload + padding,
+				s, s->msg_callback_arg);
+
+		OPENSSL_free(buffer);
+
+		if (r < 0)
+			return r;
+		}
+	else if (hbtype == TLS1_HB_RESPONSE)
+		{
+		unsigned int seq;
+
+		/* We only send sequence numbers (2 bytes unsigned int),
+		 * and 16 random bytes, so we just try to read the
+		 * sequence number */
+		n2s(pl, seq);
+
+		if (payload == 18 && seq == s->tlsext_hb_seq)
+			{
+			s->tlsext_hb_seq++;
+			s->tlsext_hb_pending = 0;
+			}
+		}
+
+	return 0;
+	}
+#endif
+
+
+/*******************************************************************************
+ * The exported Rust Function
+ *******************************************************************************/
+#if (HB_IMPL_CHOICE == RUST || HB_IMPL_CHOICE == RUST_UNSAFE)
+
+/* implemented in Rust */
+extern int __tls1_process_heartbeat(void *s, unsigned char *p, unsigned int msg_len);
+
+
+/* simple wrapper to avoid having to reinplement the very complex struct SSL */
+int tls1_process_heartbeat(SSL *s)
+{
+	unsigned char *p = &s->s3->rrec.data[0];
+	unsigned int msg_len = s->s3->rrec.length;
+
+#if HB_IMPL_CHOICE == RUST
+	return __tls1_process_heartbeat(s, p, msg_len);
+#else
+	return __tls1_process_heartbeat_no_bounds_check(s, p, msg_len);
+#endif
+}
+
+#endif
+
+/*************************************************** /LBS Project *****************************************************/
+
 
 int
 tls1_heartbeat(SSL *s)
